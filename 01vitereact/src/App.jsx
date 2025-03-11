3514376027cafff88fd88992c50e4eddaece93e3
Sanjay <sahausanjay99@gmail.com>
2025-03-11 17:52:19 +0530

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 import Chai from "./Chai"
 
 function App() {
+
+  const username = "Sanjay Gupta"
+
   return (
     <>
     <Chai/>
-    <h1>chai aur react</h1>
-    </>
+    <h1>chai aur react by {username}</h1>
+    </> 
   )
 }
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+function MyApp(){
+  return(
+    <div>
+    <h1>Custom App! </h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://google.com",
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+
+
+const anotherElement = (
+  <a href="https://google.com" target = '_blank'>VISIT GOOGLE</a>
+) 
+
+
+const anotheruser = "gopal kafle"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank' },
+  'click me to visit google ',
+  anotherElement
+)
+
 
 createRoot(document.getElementById('root')).
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
+
+
