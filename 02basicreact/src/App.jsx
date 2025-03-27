b10dbe16d2d2309b0f2219383ed5413236fef0b5
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-27 12:01:06 +0000

    commit

--- 02basicreact/src/App.jsx
+++ 02basicreact/src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import './NameList'
-import NameList from './NameList'
+import './App.css';
+import NameList from './NameList';
 
 function App() {
   return (
     <>
-    <NameList />
-    <h1>hi there, im in codespace</h1>
+      <NameList />
+      <h1>Hi there, I'm in Codespace</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 02basicreact/src/NameList.jsx
+++ 02basicreact/src/NameList.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-function NameList(){
-    const names = ['sanjay', 'gopal', 'samvai'];
-    return(
-        <>
-        <h2>{names[0]}</h2>
-        <h2>{names[1]}</h2>
-        <h2>{names[1]}</h2>
-        </>
-    )
+import Person from "./Person"; // Importing the Person component
+
+function NameList() {
+  const persons = [
+    { id: 1, name: "Amit", age: 24, skill: "JavaScript" },
+    { id: 2, name: "Priya", age: 30, skill: "Python" },
+    { id: 3, name: "Rahul", age: 21, skill: "Java" }
+  ];
+
+  return (
+    <>
+      {persons.map(person => (
+        <Person person={person} /> 
+      ))}
+    </>
+  );
 }
+
 export default NameList;
